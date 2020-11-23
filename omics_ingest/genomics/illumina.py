8468552c87d6f419bee64311536e3ec82a96913f
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-11-23 08:48:33 +0100

    :zap: More example data and adjustments

--- omics_ingest/genomics/illumina.py
+++ omics_ingest/genomics/illumina.py
@@ -32,10 +32,19 @@ from ._helpers import cleanuping
 
 #: This time should pass after the previous update and the existance of the output marker file
 #: for a run folder to be considered at rest and moved away.
-DELAY_UNTIL_AT_REST = datetime.timedelta(minutes=15)
+# DELAY_UNTIL_AT_REST = datetime.timedelta(minutes=15)
+DELAY_UNTIL_AT_REST = datetime.timedelta(seconds=15)
 
 
-def to_ingested_path(orig_path: str) -> str:
+#: AVU key to use for ``last_update`` attribute.
+KEY_LAST_UPDATE = "omics::ingest::last_update"
+#: AVU key to use destionation run folder ingestion status.
+KEY_STATUS = "omics::ingest::status"
+#: AVU key to use for ``first_seen`` attribute.
+KEY_FIRST_SEEN = "omics::ingest::first_seen"
+
+
+def to_ingested_path(orig_path: typing.Union[str, pathlib.Path]) -> pathlib.Path:
     """Convert a run folder path to an "ingested" path."""
     orig_path = pathlib.Path(orig_path)
     ingested_base = orig_path.parent.parent / (orig_path.parent.name + "-INGESTED")
@@ -85,29 +94,65 @@ def _post_runinfoxml_create_or_update(logger, session, meta):
             apply_netcopy_complete_metadata(session, netcopy_info, meta["target"])
 
 
-#: AVU key to use for ``last_update`` attribute.
-KEY_LAST_UPDATE = "omics::ingest::last_update"
-
-
 def _post_create_or_update(logger, session, meta):
-    """Update the ``last_update`` meta data value."""
+    """Update the ``last_update`` and ``status`` meta data value."""
     # Get path in irods that corresponds to root and update the meta data there.
     path = pathlib.Path(meta["path"])
     root = pathlib.Path(meta["root"])
     target = pathlib.Path(meta["target"])
-    rel_path = path.relative_to(root)
-    root_target = str(target)[: -(len(str(rel_path)) + 1)]
+    rel_root_path = path.relative_to(root)  # relative to root
+    rel_folder_path = "/".join(str(rel_root_path).split("/")[1:])  # relative to run folder
+    root_target = str(target)[: -(len(str(rel_folder_path)) + 1)]
     with cleanuping(session) as session:
         coll = session.collections.get(root_target)
-        # Remove all old meta data.
+        # Remove all old ``last_update`` meta data.
         for meta in coll.metadata.get_all(KEY_LAST_UPDATE):
             coll.metadata.remove(meta)
-        # Add new meta data.
+        # Add new ``last_update`` meta data.
         coll.metadata.add(KEY_LAST_UPDATE, datetime.datetime.now().isoformat(), "")
+        # Add new ``status`` meta data if running.
+        if not coll.metadata.get_all(KEY_STATUS):
+            coll.metadata.add(KEY_STATUS, "running", "")
 
 
-#: AVU key to use for ``first_seen`` attribute.
-KEY_FIRST_SEEN = "omics::ingest::first_seen"
+def _post_job_run_folder_done(logger, session, src_folder, dst_collection):
+    """Handle run folder being done:
+
+    - Move into ingested folder on source.
+    - Update status meta data in destination collection.
+    """
+    src_folder = pathlib.Path(src_folder)
+    # Get "last updated" time from meta data.
+    last_update = None
+    for meta in dst_collection.metadata.get_all(KEY_LAST_UPDATE):
+        value = dateutil.parser.parse(meta.value)
+        if last_update is None or value > last_update:
+            last_update = value
+    last_update_age = datetime.datetime.now() - (last_update or datetime.datetime.now())
+    # Move it data is considered at rest.
+    if last_update_age > DELAY_UNTIL_AT_REST:
+        logger.info(
+            "age of last update of %s is %s (<%s) -- will move to ingested"
+            % (dst_collection, last_update_age, DELAY_UNTIL_AT_REST)
+        )
+        new_src_folder = to_ingested_path(src_folder)
+        logger.info("attempting move %s => %s" % (src_folder, new_src_folder))
+        try:
+            new_src_folder.parent.mkdir(exist_ok=True)
+            src_folder.rename(new_src_folder)
+        except OSError as e:
+            logger.error("could not move to ingested: %s" % e)
+        # Remove all old ``status`` meta data.
+        logger.info("Marking destination collection as complete")
+        for meta in dst_collection.metadata.get_all(KEY_STATUS):
+            dst_collection.metadata.remove(meta)
+        # set ``status`` meta data to not running any more.
+        dst_collection.metadata.add(KEY_STATUS, "complete", "")
+    else:
+        logger.info(
+            "age of last update of %s is %s (<%s) -- not moving to ingest"
+            % (dst_collection, last_update_age, DELAY_UNTIL_AT_REST)
+        )
 
 
 class event_handler(Core):
@@ -121,32 +166,18 @@ class event_handler(Core):
 
     @staticmethod
     def post_job(hdlr_mod, logger, meta):
+        """Move completed run folders into the "ingested" area."""
+        src_root = pathlib.Path(meta["root"])
         with cleanuping(irods_session(hdlr_mod=hdlr_mod, meta=meta, logger=logger)) as session:
-            coll = session.collections.get(meta["target"])
-            last_update = None
-            for irods_meta in coll.metadata.get_all(KEY_LAST_UPDATE):
-                value = dateutil.parser.parse(irods_meta.value)
-                if last_update is None or value > last_update:
-                    last_update = value
-            last_update_age = datetime.datetime.now() - (last_update or datetime.datetime.now())
-            if last_update_age > DELAY_UNTIL_AT_REST:
-                logger.info(
-                    "age of last update of %s is %s (<%s) -- will move to ingest"
-                    % (meta["target"], last_update_age, DELAY_UNTIL_AT_REST)
-                )
-                root = pathlib.Path(meta["root"])
-                new_root = to_ingested_path(meta["root"])
-                logger.info("attempting move %s => %s" % (root, new_root))
-                try:
-                    new_root.parent.mkdir(exist_ok=True)
-                    root.rename(new_root)
-                except OSError as e:
-                    logger.error("could not move to ingest: %s" % e)
-            else:
-                logger.info(
-                    "age of last update of %s is %s (<%s) -- not moving to ingest"
-                    % (meta["target"], last_update_age, DELAY_UNTIL_AT_REST)
-                )
+            dst_root = session.collections.get(meta["target"])
+            dst_collections = {c.name: c for c in dst_root.subcollections}
+            for src_folder in sorted([f.name for f in sorted(src_root.iterdir())]):
+                if src_folder in dst_collections:
+                    _post_job_run_folder_done(
+                        logger, session, src_root / src_folder, dst_collections[src_folder]
+                    )
+                else:
+                    logger.info("Skipping %s as it corresponds to no destination collection")
 
     @staticmethod
     def operation(session, meta, **options):
