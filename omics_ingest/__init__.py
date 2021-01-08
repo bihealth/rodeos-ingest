ef84d374f46e67d271a4c26382ebef81ce40bc57
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-01-08 10:08:47 +0100

    :zap: Checking for external executables

--- omics_ingest/__init__.py
+++ omics_ingest/__init__.py
@@ -2,3 +2,8 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from ._check_path import _check_executables_in_path
+
+_check_executables_in_path(["hashdeep", "sort", "find"])
+del _check_executables_in_path
---
+++ omics_ingest/_check_path.py
@@ -0,0 +1,29 @@
+"""Checking for presence of required external programs."""
+
+import subprocess
+import typing
+
+
+def _executable_in_path(name: str) -> typing.Union[str, bool]:
+    """Check whether an executable with the given ``name`` is in the ``$PATH``.
+
+    Uses ``which``.
+
+    Return path to executable or ``True`` if output is empty.  Returns ``False``
+    if ``which`` could not find it.
+    """
+    res = subprocess.run(
+        ["which", name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=1, encoding="utf-8"
+    )
+    if res.returncode == 0:
+        return res.stdout or True
+    else:
+        return False
+
+
+def _check_executables_in_path(names: typing.List[str]) -> None:
+    """Checks whether/that executables in ``names`` are in the path."""
+    problematic = [name for name in names if not _executable_in_path(name)]
+    if problematic:
+        tpl = "The following executable(s) are required but not in PATH: %s"
+        raise RuntimeError(tpl % ", ".join(problematic))
