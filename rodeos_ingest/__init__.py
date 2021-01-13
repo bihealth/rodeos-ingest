658b51c334a8f726ab2878fd2575c56a20c238c1
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-01-13 12:08:39 +0100

    :abc: Not checking for executables for rtd

--- rodeos_ingest/__init__.py
+++ rodeos_ingest/__init__.py
@@ -5,5 +5,5 @@ del get_versions
 
 from ._check_path import _check_executables_in_path
 
-_check_executables_in_path(["hashdeep", "sort", "find", "ichksum", "iquest"])
-del _check_executables_in_path
+# _check_executables_in_path(["hashdeep", "sort", "find", "ichksum", "iquest"])
+# del _check_executables_in_path
