ba4e1aa4b52bc8514d45f8cd95deac83b515919f
Clemens Messerschmidt <messersc@users.noreply.github.com>
2021-11-18 16:35:04 +0100

    Docker improvements for development

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 #python \
 #    -m irods_capability_automated_ingest.irods_sync \
 #    start \
@@ -10,11 +13,18 @@
 
 iinit rods
 
-#celery -A irods_capability_automated_ingest.sync_task worker -l error -Q restart,path,file -c 4 &
+echo $(which celery)
+
+#tail -f /dev/null
+
+#celery -A irods_capability_automated_ingest.sync_task worker -l "$LOG_LEVEL" -Q restart,path,file &
+celery -A irods_capability_automated_ingest.sync_task worker -l error -Q restart,path,file -c 4 &
 
 echo $PWD
 
 env
+
+tail -f /dev/null
 
 py.test -v -x ./tests
 
